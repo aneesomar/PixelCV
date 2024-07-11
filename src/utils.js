9d905c3c1aeb251c44bf2f09a983461b7af329f1
Anees Omar <aneesomar.ao@gmail.com>
2024-07-11 14:50:22 +0200

    finished textboxes with scrolling

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,19 @@
 export function displayDialogue(text, onDisplayEnd) {
-    const dialogueUI = document.getElementById("textbox-container")
+    const dialogueUI = document.getElementById("textbox-container");
+    const dialogue = document.getElementById("dialogue")
+
+    dialogueUI.style.display = "block";
+
+    let index = 0;
+    let currentText = "";
+    const intervalRef = setInterval(() => {
+        if (index < text.length) {
+            currentText += text[index];
+            dialogue.innerHTML = currentText;
+            index++
+            return;
+        }
+    })
+
+
 }
