fc0e556fea4139519816c0f5daaece0a62b3d86e
Anees Omar <aneesomar.ao@gmail.com>
2024-07-15 13:31:43 +0200

    fixed grammer on captions

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    exit: 'If you want to exit, just close the tab',
-    Uni: 'I attend the University of Cape Town and study Geomatics in the Computer Science Stream ',
-    gitHub: 'Here is a link to my <a href="https://github.com/aneesomar" target="_blank">Github</a> where you can find the repository for this project aswell! ',
-    paper: 'You can view my CV by clicking <a href="https://docs.google.com/document/d/1uQfz1Au0gVKx4xPT8Xa48dg2FAsx0ErqV3pJp6xuOWo/edit?usp=sharing" target="_blank"" download>here</a>',
-    tent: 'I love outdoor activities such as hiking, camping and climbing , I also going to the gym',
-    bookShelf: 'Click <a href="www.linkedin.com/in/anees-omar-648a26266/" target="_blank">here</a> to see my LinkedIn '
+    exit: "To exit, simply close the tab.",
+    Uni: "I attend the University of Cape Town, studying Geomatics in the Computer Science stream.",
+    gitHub: "Here is a link to my <a href=https://github.com/aneesomar target=_blank>GitHub</a> where you can also find the repository for this project!",
+    paper: "You can view my CV by clicking <a href=https://docs.google.com/document/d/1uQfz1Au0gVKx4xPT8Xa48dg2FAsx0ErqV3pJp6xuOWo/edit?usp=sharing target=_blank download>here</a>.",
+    tent: "I have a passion for outdoor adventures, including hiking through scenic trails, camping under the stars, and climbing challenging terrains. Additionally, I enjoy regularly working out at the gym.",
+    bookShelf: "Click <a href=https://www.linkedin.com/in/anees-omar-648a26266/ target=_blank>here</a> to view my LinkedIn profile."
 
 }
