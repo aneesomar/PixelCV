731512b648eef451790bd8ed1d4059c6fa836763
Anees Omar <aneesomar.ao@gmail.com>
2024-07-15 14:17:00 +0200

    fixed links

--- src/constants.js
+++ src/constants.js
@@ -3,9 +3,9 @@ export const scaleFactor = 4;
 export const dialogueData = {
     exit: 'To exit, simply close the tab.',
     Uni: 'I attend the University of Cape Town, studying Geomatics in the Computer Science stream.',
-    gitHub: 'Here is a link to my <a href="https://github.com/aneesomar" target="_blank>GitHub</a>" where you can also find the repository for this project!',
+    gitHub: 'Here is a link to my <a href="https://github.com/aneesomar" target="_blank">GitHub</a> where you can also find the repository for this project!',
     paper: 'You can view my CV by clicking <a href="https://docs.google.com/document/d/1uQfz1Au0gVKx4xPT8Xa48dg2FAsx0ErqV3pJp6xuOWo/edit?usp=sharing" target="_blank download">here</a>.',
     tent: 'I have a passion for outdoor adventures, including hiking through scenic trails, camping under the stars, and climbing challenging terrains. Additionally, I enjoy regularly working out at the gym.',
-    bookShelf: 'Click <a href="https://www.linkedin.com/in/anees-omar-648a26266/" target="_blank>here"</a> to view my LinkedIn profile.'
+    bookShelf: 'Click <a href="https://www.linkedin.com/in/anees-omar-648a26266/" target="_blank>here">here</a> to view my LinkedIn profile.'
 
 }
