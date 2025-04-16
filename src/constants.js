ef8aa49e6cf16009cdc86f6803759b4796f2059b
Anees Omar <aneesomar.ao@gmail.com>
2025-04-16 13:58:54 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ export const dialogueData = {
     exit: 'To exit, simply close the tab.',
     Uni: 'I attend the University of Cape Town, studying Geomatics in the Computer Science stream.',
     gitHub: 'Here is a link to my <a href="https://github.com/aneesomar" target="_blank">GitHub</a> where you can also find the repository for this project!',
-    paper: 'You can view my CV by clicking <a href="https://aneesomarcv.tiiny.site/">here</a>.',
+    paper: 'You can view my CV by clicking <a href="https://aneesomarcv.tiiny.site/" target="_blank download">here</a>.',
     tent: 'I have a passion for outdoor adventures, including hiking through scenic trails, camping under the stars, and climbing challenging terrains. Additionally, I enjoy regularly working out at the gym.',
     bookShelf: 'Click <a href="https://www.linkedin.com/in/anees-omar-648a26266/" target="_blank>here">here</a> to view my LinkedIn profile.'
 
