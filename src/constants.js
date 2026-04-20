973ccea62402e7c35ecd70532c5865f3253a44fd
Anees Omar <aneesomar.ao@gmail.com>
2026-04-20 16:24:56 +0200

    Modify dialogueData for education and interests
    Updated dialogueData to reflect current education and work status.

--- src/constants.js
+++ src/constants.js
@@ -2,10 +2,10 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
     exit: 'To exit, simply close the tab.',
-    Uni: 'I attend the University of Cape Town, studying Geomatics in the Computer Science stream.',
+    Education: 'I have a degree in Geomatics in the Computer Science stream and curently work as a smart GIS Analyst at Mission For Inner City',
     gitHub: 'Here is a link to my <a href="https://github.com/aneesomar" target="_blank">GitHub</a> where you can also find the repository for this project!',
     paper: 'You can view my CV by clicking <a href="https://aneesomarcv.tiiny.site/" target="_blank download">here</a>.',
-    tent: 'I have a passion for outdoor adventures, including hiking through scenic trails, camping under the stars, and climbing challenging terrains. Additionally, I enjoy regularly working out at the gym.',
+    tent: 'I have a passion for outdoor adventures, including hiking through scenic trails, camping under the stars, and climbing challenging terrains.',
     bookShelf: 'Click <a href="https://www.linkedin.com/in/anees-omar-648a26266/" target="_blank>here">here</a> to view my LinkedIn profile.'
 
 }
