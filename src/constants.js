a511a4c11f5e9efbf115c01012f0ef32b2d9c2f5
Anees Omar <aneesomar.ao@gmail.com>
2026-04-21 11:06:59 +0200

    Fix typo in dialogueData key from 'uni' to 'Uni'

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
     exit: 'To exit, simply close the tab.',
-    uni: 'I have a degree in Geomatics and Computer Science and love to work with spatial data!',
+    Uni: 'I have a degree in Geomatics and Computer Science and love to work with spatial data!',
     gitHub: 'Here is a link to my <a href="https://github.com/aneesomar" target="_blank">GitHub</a> where you can also find the repository for this project and many many more!',
     paper: 'just a desk..',
     tent: 'I have a passion for outdoor adventures, including hiking through scenic trails, camping under the stars, and climbing challenging terrains.',
