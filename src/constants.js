6dd69660718c36131ad4cf26e7deac48b8eb025f
Anees Omar <aneesomar.ao@gmail.com>
2026-04-21 11:01:38 +0200

    Update dialogue data with new personal information

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,9 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
     exit: 'To exit, simply close the tab.',
-    Education: 'I have a degree in Geomatics in the Computer Science stream and curently work as a smart GIS Analyst at Mission For Inner City',
-    gitHub: 'Here is a link to my <a href="https://github.com/aneesomar" target="_blank">GitHub</a> where you can also find the repository for this project!',
-    paper: 'You can view my CV by clicking <a href="https://aneesomarcv.tiiny.site/" target="_blank download">here</a>.',
+    uni: 'I have a degree in Geomatics and Computer Science and love to work with spatial data!',
+    gitHub: 'Here is a link to my <a href="https://github.com/aneesomar" target="_blank">GitHub</a> where you can also find the repository for this project and many many more!',
+    paper: 'just a desk..',
     tent: 'I have a passion for outdoor adventures, including hiking through scenic trails, camping under the stars, and climbing challenging terrains.',
     bookShelf: 'Click <a href="https://www.linkedin.com/in/anees-omar-648a26266/" target="_blank>here">here</a> to view my LinkedIn profile.'
 
